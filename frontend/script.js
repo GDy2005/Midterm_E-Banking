88d87fca89af687e5ac511a9721c8792df600e4e
GDy2005 <lizzbiz2005@gmail.com>
2025-10-29 00:13:46 +0700

    Add page transition in login

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,17 @@
 // Login function
 function login() {
-    const user = document.getElementById("username").value;
+    const user = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value;
 
-    if (user == "admin" && password == "1") {
-        window.location.href = "payment.html"
+    if (user === "admin" && password === "1") {
+        const page = document.getElementById("page-transition");
+        page.classList.add("fade-out");
+
+        setTimeout(() => {
+            window.location.href = "payment.html";
+        }, 600);
     } else {
-        alert("Login failed")
+        alert("Login failed");
     }
 
     return false;
