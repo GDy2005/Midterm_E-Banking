62e8b6a68953f560de2ebfcf4adf376fd3f1079a
NgoThuan <ngochithuan.dev@gmail.com>
2025-11-06 13:22:50 +0700

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE IF NOT EXISTS db_ebank;
+USE db_ebank;
+
+-- DROP DATABASE db_ebank;
+
+CREATE TABLE Customer (
+    CustomerID INT NOT NULL AUTO_INCREMENT,
+    UserName VARCHAR(25) NOT NULL,
+    HashPassword VARCHAR(25) NOT NULL,
+    FullName VARCHAR(50) CHARACTER SET utf8mb4 NOT NULL,
+    Balance INT NOT NULL DEFAULT 0,
+    Email VARCHAR(50) NOT NULL,
+    PhoneNumber VARCHAR(12) NOT NULL,
+    
+    CONSTRAINT PK_Customer PRIMARY KEY(CustomerID),
+    CONSTRAINT CHK_Balance CHECK (Balance >= 0)
+);
+
+CREATE TABLE Student (
+    StudentID INT NOT NULL,
+    FullName VARCHAR(50) CHARACTER SET utf8mb4 NOT NULL,
+    Email VARCHAR(50) NOT NULL,
+    
+    CONSTRAINT PK_Student PRIMARY KEY(StudentID)
+);
+
+CREATE TABLE Tution (
+    TutionID INT NOT NULL AUTO_INCREMENT,
+    StudentID INT NOT NULL,
+    Semester VARCHAR(25) CHARACTER SET utf8mb4 NOT NULL,
+    Fee INT NOT NULL DEFAULT 0,
+    BeginDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    EndDate TIMESTAMP NOT NULL,
+    
+    CONSTRAINT PK_Tution PRIMARY KEY(TutionID),
+    CONSTRAINT CHK_Fee CHECK (Fee >= 0),
+    CONSTRAINT FK_Tution_Student FOREIGN KEY (StudentID) REFERENCES Student(StudentID)
+);
+
+CREATE TABLE `Transaction` (
+    TransactionID INT NOT NULL AUTO_INCREMENT,
+    CustomerID INT NOT NULL,
+    TutionID INT NOT NULL,
+    PaidAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `Status` ENUM('PAID','UNPAID','CANCELLED') NOT NULL,
+    
+    CONSTRAINT PK_Transaction PRIMARY KEY(TransactionID),
+    CONSTRAINT FK_Transaction_Customer FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID),
+    CONSTRAINT FK_Transaction_Tution FOREIGN KEY (TutionID) REFERENCES Tution(TutionID)
+);
